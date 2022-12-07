1b6a599a8ce8cec5e9c42b9b779b83ad5a955861
theconfusedindian <schandrapatla.ca@gmail.com>
2022-12-07 11:11:21 -0800

    add functionality to privacy statement and email

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -7,6 +7,17 @@ import FB from "../../assets/icons/Icon-facebook.svg";
 import Twitter from "../../assets/icons/Icon-twitter.svg";
 
 export default function Footer() {
+  const openInNewTab = (url) => {
+    window.open(url, "_blank", "noopener,noreferrer");
+  };
+
+  const sendEmail = (mailto) => {
+    var mail = "mailto:yogi.harpreet@gmail.com";
+    var a = document.createElement("a");
+    a.href = mail;
+    a.click();
+  };
+
   return (
     <div className="siteFooter">
       <div className="siteFooter__title">
@@ -31,7 +42,15 @@ export default function Footer() {
         <p className="siteFooter__contact--yogi">Yogi Harpreet Vohra</p>
         {/* <p className="siteFooter__contact--address">304-1260 Nelson St</p>
         <p className="siteFooter__contact--address">Vancouver BC V6E 1J7</p> */}
-        <p className="siteFooter__contact--email">yogi.harpreet@gmail.com</p>
+        <button
+          className="siteFooter__contact--email"
+          onClick={(e) => {
+            e.preventDefault();
+            sendEmail("yogi.harpreet@gmail.com");
+          }}
+        >
+          yogi.harpreet@gmail.com
+        </button>
         <p className="siteFooter__contact--phone">+1 (604) 721-6322</p>
       </div>
       <div className="siteFooter__other">
@@ -46,8 +65,22 @@ export default function Footer() {
       </div>
       <div className="siteFooter__credits">
         <p className="siteFooter__credits--cr">©2022 Saral-Yoga</p>
-        <p className="siteFooter__credits--cr">Privacy Policy</p>
+        <button
+          type="button"
+          className="siteFooter__credits--cr"
+          onClick={(e) => {
+            e.preventDefault();
+            window.location.href = openInNewTab(
+              "https://www.privacypolicies.com/live/a4f9814e-f036-4b1a-a1dc-8ca431f9d9aa"
+            );
+          }}
+        >
+          Privacy Policy
+        </button>
+        {/* <p className="siteFooter__credits--cr">Privacy Policy</p> */}
       </div>
     </div>
   );
 }
+
+// Privacy policy: https://www.privacypolicies.com/live/a4f9814e-f036-4b1a-a1dc-8ca431f9d9aa
